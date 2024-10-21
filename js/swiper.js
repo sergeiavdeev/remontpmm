6f1653aadc4eee20d54e931a90435576e4c5829d
sergeiavdeev <sergei_avdeev@mail.ru>
2024-10-21 16:16:46 +0300

    Autoplay galary

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,11 @@
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
